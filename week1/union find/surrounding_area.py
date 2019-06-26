59f20d9532953543691e2306eba4b28d6ab18d8d
elaneyc <echeng5@wellesley.edu>
2019-06-25 20:52:42 -0700

    add surrounding area

---
+++ week1/union find/surrounding_area.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def solve(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: None Do not return anything, modify board in-place instead.
+        """
+       # I wasn't able to solve this..... I'll keep working on it! But I ran out of time
