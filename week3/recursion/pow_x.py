ede332b6a27e9131ef7d1765a061ed5ee538cf8c
echeng5@wellesley.edu <echeng5@wellesley.edu>
2019-07-08 17:20:06 -0700

    update pow

--- week3/recursion/pow_x.py
+++ week3/recursion/pow_x.py
@@ -9,11 +9,10 @@ class Solution(object):
             return 1
         elif n < 0:
             return 1/self.myPow(x, -n)
+
         temp = self.myPow(x, n / 2)
 
         if n % 2 == 0:
             return temp * temp
         elif n > 0:
             return x * temp * temp
-        else:
-            return (temp * temp) / x
