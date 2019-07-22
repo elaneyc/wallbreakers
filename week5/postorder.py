b7e051a85b584fe6b34b6f262f5ada54a4818db2
echeng5@wellesley.edu <echeng5@wellesley.edu>
2019-07-22 08:29:37 -0700

    add postorder

---
+++ week5/postorder.py
@@ -0,0 +1,30 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+class Solution(object):
+    def postorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+        if not root.children:
+            return [root.val]
+
+        result = []
+        for c in root.children:
+            result += self.postorder(c)
+
+        result.append(root.val)
+        return result
+
+
+
+
+
+        
